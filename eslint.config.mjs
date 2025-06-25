171935f54ca1ee99f309873916762633f636ca70
Fernanda <fernandakagami@gmail.com>
2025-06-25 19:06:23 -0300

    Create project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,47 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: [
+			"next",
+			"next/core-web-vitals",
+			"next/typescript",
+			"plugin:prettier/recommended",
+			"plugin:jsx-a11y/recommended",
+		],
+		plugins: ["prettier", "jsx-a11y"],
+		rules: {
+			"prettier/prettier": [
+				"error",
+				{
+					tabWidth: 2,
+					endOfLine: "auto",
+					printWidth: 120,
+					singleQuote: false,
+					trailingComma: "all",
+					arrowParens: "always",
+					bracketSpacing: true,
+					semi: true,
+					useTabs: true,
+					plugins: ["prettier-plugin-tailwindcss"],
+				},
+				{
+					usePrettierrc: false,
+				},
+			],
+			"react/react-in-jsx-scope": "off",
+			"jsx-a11y/alt-text": "warn",
+			"jsx-a11y/aria-props": "warn",
+			"jsx-a11y/aria-proptypes": "warn",
+			"jsx-a11y/aria-unsupported-elements": "warn",
+			"jsx-a11y/role-has-required-aria-props": "warn",
+			"jsx-a11y/role-supports-aria-props": "warn",
+		},
+	}),
 ];
 
 export default eslintConfig;
